a812b02bb63db48bcad9bf03a37c307fe51c4ebd
anBarot <barot.anne@gmail.com>
2023-04-19 10:55:05 +0200

    create bubbletea svg

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,33 @@ import './App.css';
 import React from 'react'
 import Stack from '@mui/material/Stack'
 import Box from '@mui/material/Box'
-import Grid2 from '@mui/material/Unstable_Grid2'
 import Cup from "./components/Cup"
 import Name from "./components/Name"
 import Gelée from "./components/Gelée"
 import Tea from "./components/Tea"
 import Size from "./components/Size"
+import Bubbletea from './components/Bubbletea';
 
 function AppStack(props: {name: string, component: any}) {
 
 	return (
 		<Stack direction="row" justifyContent={'space-between'}>
-			<h1>{props.name}</h1>{props.component}
+			<h2>{props.name}</h2>{props.component}
 		</Stack>
 	); 
 
 }
 
 export default function App() {
-	
+
 	return (
 		<Box id="app-box">
 			<Stack direction="row" id="svg-selector">
-				<Box id="svg">
-					<Box id="svg-up">
-					</Box>
-					<Box id="svg-down">
-					</Box>
+				<Box id="bubbletea">
+					<Stack spacing={5}>
+						<h1>name</h1>
+						<Bubbletea />
+					</Stack>
 				</Box>
 				<Box id="selector">
 					<Stack spacing={10} id="selector-stack">
